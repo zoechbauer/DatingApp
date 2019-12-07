8882afd9972d944d4bd04fe61ed6c98f294236df
Zoechbauer <hans.zoechbauer@aon.at>
2019-12-07 18:23:36 +0100

    End of Section 9

---
+++ DatingApp-SPA/src/app/members/member-detail/member-detail.component.ts
@@ -0,0 +1,52 @@
+import { Component, OnInit, Input } from '@angular/core';
+import { UserService } from 'src/app/_Services/User.service';
+import { AlertifyService } from 'src/app/_Services/alertify.service';
+import { User } from 'src/app/_models/user';
+import { ActivatedRoute } from '@angular/router';
+import { NgxGalleryOptions, NgxGalleryImage, NgxGalleryAnimation } from 'ngx-gallery';
+import { NgForOf } from '@angular/common';
+
+@Component({
+  selector: 'app-member-detail',
+  templateUrl: './member-detail.component.html',
+  styleUrls: ['./member-detail.component.css']
+})
+export class MemberDetailComponent implements OnInit {
+  user: User;
+  galleryOptions: NgxGalleryOptions[];
+  galleryImages: NgxGalleryImage[];
+
+  constructor(private userService: UserService, private alertify: AlertifyService,
+              private route: ActivatedRoute) { }
+
+  ngOnInit() {
+    this.route.data.subscribe( data => {
+      this.user = data['user'];
+      }
+    );
+
+    this.galleryOptions = [
+      {
+        width: '500px',
+        height: '500px',
+        imagePercent: 100,
+        thumbnailsColumns: 4,
+        imageAnimation: NgxGalleryAnimation.Slide,
+        preview: false
+      }
+    ];
+    this.galleryImages = this.getImages();
+  }
+
+  getImages() {
+    const imageUrls = [];
+    for (const photo of this.user.photos) {
+      imageUrls.push( {
+        small: photo.url,
+        medium: photo.url,
+        big: photo.url
+      });
+    }
+    return imageUrls;
+  }
+}
